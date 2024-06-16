cbc2dce77857388fc15c1cb8cba59b2580ab71e5
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-06-16 09:30:43 +0700

    print double quotes

--- 1.hello-world/hello-world.c
+++ 1.hello-world/hello-world.c
@@ -2,7 +2,12 @@
 
 int main(void)
 {
-  printf("hello world");
+  // \n = newline
+  // \t = tab
+  printf("hello world\n");
+
+  // print double quotes in C
+  printf("\"print double quotes\" - C language");
 
   return 0;
 }
