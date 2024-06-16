782e1902fb434de4036d15f220951e6bc2d40b3b
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-06-16 11:43:07 +0700

    augmented assignment operators

--- 4.aritmetics-variable/aritmetics-variable.c
+++ 4.aritmetics-variable/aritmetics-variable.c
@@ -29,7 +29,17 @@ int main()
   int d = b / c;
   printf("d value: %d\n", d); // output: 2, should 2.5
   float e = b / (float)c;
-  printf("e value: %.1f", e); // output: 2.5000
+  printf("e value: %.1f\n", e); // output: 2.5000
+
+  // augmented assignment operators
+  int f = 10;
+  f += 10; // output 20
+  f -= 5;  // output 15
+  f *= 2;  // output 30
+  f /= 10; // output 3
+  f %= 2;  // output 1
+
+  printf("f value : %d\n", f); // otuput 1
 
   return 0;
 }
